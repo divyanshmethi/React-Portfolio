f82e2f726dd13c3d489a9dc120f77b180cd53954
Divyansh.Methi <Divyansh_Methi@dell.com>
2020-09-12 12:17:15 +0530

    Updates

---
+++ precache-manifest.87c1affcd97597cec8d97e9355a8bac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6789fa86b9c4a2fcea6be82177f11694",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "69577eb7260170c56dc8",
+    "url": "/React-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a3cf057da6f0ab5d19db",
+    "url": "/React-Portfolio/static/js/2.f3b6c8e1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/React-Portfolio/static/js/2.f3b6c8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69577eb7260170c56dc8",
+    "url": "/React-Portfolio/static/js/main.3febae5b.chunk.js"
+  },
+  {
+    "revision": "53cd5964469ae1da263b",
+    "url": "/React-Portfolio/static/js/runtime-main.01560ae1.js"
+  }
+]);
