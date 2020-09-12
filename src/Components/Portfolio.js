ce683367d36ea9ad730f41064b7b3b7355f68691
Divyansh.Methi <Divyansh_Methi@dell.com>
2020-09-12 14:16:34 +0530

    First Draft

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,25 +3,25 @@ import React, { Component } from 'react';
 class Portfolio extends Component {
   render() {
 
-    // if(this.props.data){
-    //   var projects = this.props.data.projects.map(function(projects){
-    //     var projectImage = 'images/portfolio/'+projects.image;
-    //     return <div key={projects.title} className="columns portfolio-item">
-    //        <div className="item-wrap">
-    //         <a href={projects.url} title={projects.title}>
-    //            <img alt={projects.title} src={projectImage} />
-    //            <div className="overlay">
-    //               <div className="portfolio-item-meta">
-    //              <h5>{projects.title}</h5>
-    //                  <p>{projects.category}</p>
-    //               </div>
-    //             </div>
-    //           <div className="link-icon"><i className="fa fa-link"></i></div>
-    //         </a>
-    //       </div>
-    //     </div>
-    //   })
-    // }
+    if(this.props.data){
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+    }
 
     return (
       <section id="portfolio">
@@ -33,9 +33,7 @@ class Portfolio extends Component {
             <h1>Check Out Some of My Works.</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                <a href="https://forms.gle/gsJgxjUoCqy12N3Y6" target="_blank">
-                  <img src="images/autism.PNG"/>
-                </a>
+                {projects}
             </div>
           </div>
       </div>
